db22f7ba434946de8abe335b3cdcca6583eef97c
Bogdan Cuza <bogdancuza@protonmail.com>
2022-06-14 21:40:18 +0300

    Add greeting

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,14 @@
 require_relative '../config/environment'
 
+def greet
+    puts <<-'EOF'
+    ___             _   _                           
+    | __|__  ___  __| | | |   ___  __ _ __ _ ___ _ _ 
+    | _/ _ \/ _ \/ _` | | |__/ _ \/ _` / _` / -_) '_|
+    |_|\___/\___/\__,_| |____\___/\__, \__, \___|_|  
+                                  |___/|___/                        
+    
+    EOF
+end
 
-
-puts "HELLO WORLD"
+greet
